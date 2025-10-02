562eba108e0158d47f2c98c5165ecb9bf016e2cd
Fernando <mkiii@hotmail.es>
2025-10-03 01:11:32 +0200

    initial commit, project set up

---
+++ build/CMakeFiles/GameBoyEmu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameBoyEmu.dir/src/main.cpp.o"
+  "CMakeFiles/GameBoyEmu.dir/src/main.cpp.o.d"
+  "GameBoyEmu"
+  "GameBoyEmu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameBoyEmu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
