48b639e53d8bf6df5a24534e80a241d153d265a1
Fernando <mkiii@hotmail.es>
2025-10-03 16:39:16 +0200

    ignoring build

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,10 @@
 #include "instructionSet.h"
 
 int main(){
+
     initializeInstructionSet();
-    // std::cout << "Hello, from GameBoyEmu!\n";
 
-    // std::ifstream file("../resources/instructionSet.json");
-    // Json data = Json::parse(file);
-    // data = data["unprefixed"];
-    // int numero = data["0x00"]["cycles"].at(0);
-    // std::cout << numero;
 
-    
 
     return 0;
 }
